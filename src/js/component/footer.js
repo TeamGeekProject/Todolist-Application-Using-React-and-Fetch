c78bcaaec1a63b3ebcaae466e65689aac2f3ff3a
Johanna Castillo <122300795+joaarg86@users.noreply.github.com>
2023-04-16 20:24:41 -0300

    johanna

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
+import {Link} from "react-router-dom";
 
 export const Footer = () => (
   <footer className="footer mt-auto py-3 text-center">
+    <Link to={{ pathname: " " }} target="_blank">
+      <button className="btn btn-secondary">Click to open new tab</button></Link>
     <p>
-      We use a template from{" "}
-      <a href="https://github.com/4GeeksAcademy/react-hello-webapp">
+      We use a template from</p>
+      <Link to={{pathname: "//github.com/4geeksacademy"}} target="_blank">
         4Geeks Academy
-      </a>{" "}
+      </Link>
       <i className="fa fa-heart text-danger" />
-    </p>
+    
   </footer>
 );
